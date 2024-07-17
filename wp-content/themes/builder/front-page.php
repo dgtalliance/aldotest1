c1190fec2536208b129662feda78c308cc8af6be
LaricoDGT <larico@dgtalliance.com>
2024-07-17 11:34:11 -0500

    home

--- wp-content/themes/builder/front-page.php
+++ wp-content/themes/builder/front-page.php
@@ -12,131 +12,8 @@ $temporalImage = builder_get_assets_path()."/images/temp.png";
 
         <div class="slide">
 
-            <div class="item">
-                <div class="agent">
-                    <h1>Matthew Martinez</h1>
-                    <h2>Top Luxury Real Estate Agent</h2>
-                    <img class='avatar' src="<?php echo get_template_directory_uri(); ?>/assets/images/home/Matt.png"
-                        alt="">
-                    <div class="right-info">
-                        <div class="infos">
-                            <div class="info">
-                                <h3>Lorem</h3>
-                                <p>Lorem ipsum dolor</p>
-                            </div>
-                            <div class="info">
-                                <h3>Lorem Ipsum</h3>
-                                <p>Lorem ipsum dolor</p>
-                            </div>
-                            <div class="info">
-                                <h3>Sample Text</h3>
-                                <p>Lorem ipsum dolor</p>
-                            </div>
-                        </div>
-                        <div class="socials">
-                            <a href="#" class="social-icon i-prime-twitter" title="X" target="_blank" rel="nofollow">
-                                <span>Facebook</span>
-                            </a>
-                            <a href="#" class="social-icon i-prime-facebook" title="facebook" target="_blank"
-                                rel="nofollow">
-                                <span>Twitter</span>
-                            </a>
-                            <a href="#" class="social-icon i-prime-instagram" title="Instagram" target="_blank"
-                                rel="nofollow">
-                                <span>Instagram</span>
-                            </a>
-
-                        </div>
-                    </div>
-
-                </div>
-                <a href="#" class="more"><span>More</span></a>
-            </div>
 
 
-
-            <div class="item">
-                <div class="agent">
-                    <h1>Matthew Martinez</h1>
-                    <h2>Top Luxury Real Estate Agent</h2>
-                    <img class='avatar' src="<?php echo get_template_directory_uri(); ?>/assets/images/home/Matt.png"
-                        alt="">
-                    <div class="right-info">
-                        <div class="infos">
-                            <div class="info">
-                                <h3>Lorem</h3>
-                                <p>Lorem ipsum dolor</p>
-                            </div>
-                            <div class="info">
-                                <h3>Lorem Ipsum</h3>
-                                <p>Lorem ipsum dolor</p>
-                            </div>
-                            <div class="info">
-                                <h3>Sample Text</h3>
-                                <p>Lorem ipsum dolor</p>
-                            </div>
-                        </div>
-                        <div class="socials">
-                            <a href="#" class="social-icon i-prime-twitter" title="X" target="_blank" rel="nofollow">
-                                <span>Facebook</span>
-                            </a>
-                            <a href="#" class="social-icon i-prime-facebook" title="facebook" target="_blank"
-                                rel="nofollow">
-                                <span>Twitter</span>
-                            </a>
-                            <a href="#" class="social-icon i-prime-instagram" title="Instagram" target="_blank"
-                                rel="nofollow">
-                                <span>Instagram</span>
-                            </a>
-
-                        </div>
-                    </div>
-
-                </div>
-                <a href="#" class="more"><span>More</span></a>
-            </div>
-
-
-            <div class="item">
-                <div class="agent">
-                    <h1>Matthew Martinez</h1>
-                    <h2>Top Luxury Real Estate Agent</h2>
-                    <img class='avatar' src="<?php echo get_template_directory_uri(); ?>/assets/images/home/Matt.png"
-                        alt="">
-                    <div class="right-info">
-                        <div class="infos">
-                            <div class="info">
-                                <h3>Lorem</h3>
-                                <p>Lorem ipsum dolor</p>
-                            </div>
-                            <div class="info">
-                                <h3>Lorem Ipsum</h3>
-                                <p>Lorem ipsum dolor</p>
-                            </div>
-                            <div class="info">
-                                <h3>Sample Text</h3>
-                                <p>Lorem ipsum dolor</p>
-                            </div>
-                        </div>
-                        <div class="socials">
-                            <a href="#" class="social-icon i-prime-twitter" title="X" target="_blank" rel="nofollow">
-                                <span>Facebook</span>
-                            </a>
-                            <a href="#" class="social-icon i-prime-facebook" title="facebook" target="_blank"
-                                rel="nofollow">
-                                <span>Twitter</span>
-                            </a>
-                            <a href="#" class="social-icon i-prime-instagram" title="Instagram" target="_blank"
-                                rel="nofollow">
-                                <span>Instagram</span>
-                            </a>
-
-                        </div>
-                    </div>
-
-                </div>
-                <a href="#" class="more"><span>More</span></a>
-            </div>
 
 
             <div class="item">
@@ -177,9 +54,92 @@ $temporalImage = builder_get_assets_path()."/images/temp.png";
 
                         </div>
                     </div>
-
+                    <a href="#" class="more"><span>More</span></a>
                 </div>
-                <a href="#" class="more"><span>More</span></a>
+            </div>
+
+            <div class="item">
+                <div class="agent">
+                    <div class="name">
+                        <h1>Matthew Martinez</h1>
+                        <h2>Top Luxury Real Estate Agent</h2>
+                    </div>
+                    <img class='avatar' src="<?php echo get_template_directory_uri(); ?>/assets/images/home/Matt.png"
+                        alt="">
+                    <div class="right-info">
+                        <div class="infos">
+                            <div class="info">
+                                <h3>Lorem</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
+                            <div class="info">
+                                <h3>Lorem Ipsum</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
+                            <div class="info">
+                                <h3>Sample Text</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
+                        </div>
+                        <div class="socials">
+                            <a href="#" class="social-icon i-prime-twitter" title="X" target="_blank" rel="nofollow">
+                                <span>x</span>
+                            </a>
+                            <a href="#" class="social-icon i-prime-facebook" title="facebook" target="_blank"
+                                rel="nofollow">
+                                <span>facebook</span>
+                            </a>
+                            <a href="#" class="social-icon i-prime-instagram" title="Instagram" target="_blank"
+                                rel="nofollow">
+                                <span>Instagram</span>
+                            </a>
+
+                        </div>
+                    </div>
+                    <a href="#" class="more"><span>More</span></a>
+                </div>
+            </div>
+
+            <div class="item">
+                <div class="agent">
+                    <div class="name">
+                        <h1>Matthew Martinez</h1>
+                        <h2>Top Luxury Real Estate Agent</h2>
+                    </div>
+                    <img class='avatar' src="<?php echo get_template_directory_uri(); ?>/assets/images/home/Matt.png"
+                        alt="">
+                    <div class="right-info">
+                        <div class="infos">
+                            <div class="info">
+                                <h3>Lorem</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
+                            <div class="info">
+                                <h3>Lorem Ipsum</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
+                            <div class="info">
+                                <h3>Sample Text</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
+                        </div>
+                        <div class="socials">
+                            <a href="#" class="social-icon i-prime-twitter" title="X" target="_blank" rel="nofollow">
+                                <span>x</span>
+                            </a>
+                            <a href="#" class="social-icon i-prime-facebook" title="facebook" target="_blank"
+                                rel="nofollow">
+                                <span>facebook</span>
+                            </a>
+                            <a href="#" class="social-icon i-prime-instagram" title="Instagram" target="_blank"
+                                rel="nofollow">
+                                <span>Instagram</span>
+                            </a>
+
+                        </div>
+                    </div>
+                    <a href="#" class="more"><span>More</span></a>
+                </div>
             </div>
 
             <?php /* 
