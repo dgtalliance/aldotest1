58e3f601753dbe0feaebb6adfd3c9bcae1138109
LaricoDGT <larico@dgtalliance.com>
2024-07-24 11:18:39 -0500

    home mobile

--- wp-content/themes/builder/front-page.php
+++ wp-content/themes/builder/front-page.php
@@ -49,16 +49,64 @@ $temporalImage = builder_get_assets_path()."/images/temp.png";
                 <div class="zi-2 info-area">
                     <div class="infos">
                         <div class="info">
-                            <h3>Lorem</h3>
-                            <p>Lorem ipsum dolor</p>
+
+                            <div class="mobile">
+                                <span class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                        <g fill="none" stroke="currentColor" stroke-linecap="round"
+                                            stroke-linejoin="round" stroke-width="1.5" color="currentColor">
+                                            <path
+                                                d="M2 14c0-3.507 0-5.26.908-6.44q.25-.325.554-.592C4.57 6 6.212 6 9.5 6h5c3.288 0 4.931 0 6.038.968q.304.268.554.592C22 8.74 22 10.493 22 14s0 5.26-.908 6.44a4 4 0 0 1-.554.591C19.43 22 17.788 22 14.5 22h-5c-3.287 0-4.931 0-6.038-.968a4 4 0 0 1-.554-.592C2 19.26 2 17.507 2 14m14-8c0-1.886 0-2.828-.586-3.414S13.886 2 12 2s-2.828 0-3.414.586S8 4.114 8 6" />
+                                            <path
+                                                d="M12 11c-1.105 0-2 .672-2 1.5s.895 1.5 2 1.5s2 .672 2 1.5s-.895 1.5-2 1.5m0-6c.87 0 1.612.417 1.886 1M12 11v-1m0 7c-.87 0-1.612-.417-1.886-1M12 17v1m-6-6H2m20 0h-4" />
+                                        </g>
+                                    </svg>
+                                </span>
+
+                                <strong>150</strong>
+                            </div>
+                            <div class="desktop">
+                                <h3>Lorem</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
                         </div>
                         <div class="info">
-                            <h3>Lorem Ipsum</h3>
-                            <p>Lorem ipsum dolor</p>
+                            <div class="mobile">
+                                <span class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                        <g fill="none" stroke="currentColor" stroke-linecap="round"
+                                            stroke-linejoin="round" stroke-width="1.5" color="currentColor">
+                                            <path d="M4.5 9.5a7.5 7.5 0 1 0 15 0a7.5 7.5 0 0 0-15 0" />
+                                            <path
+                                                d="M9 10.167s.75 0 1.5 1.333c0 0 2.382-3.333 4.5-4m1.883 7.5l.67 3.21c.43 2.062.646 3.093.203 3.582c-.442.489-1.21.069-2.746-.772l-2.274-1.245c-.363-.198-.544-.298-.736-.298s-.373.1-.736.298L8.99 21.02c-1.536.84-2.304 1.261-2.746.772s-.227-1.52.203-3.582l.67-3.21" />
+                                        </g>
+                                    </svg>
+                                </span>
+                                <strong>152</strong>
+                            </div>
+
+                            <div class="desktop">
+                                <h3>Lorem</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
                         </div>
                         <div class="info">
-                            <h3>Sample Text</h3>
-                            <p>Lorem ipsum dolor</p>
+                            <div class="mobile">
+                                <span class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                        <path fill="none" stroke="currentColor" stroke-linecap="round"
+                                            stroke-linejoin="round" stroke-width="1.5"
+                                            d="M13.5 19.5v-8c0-.943 0-1.414-.293-1.707S12.443 9.5 11.5 9.5s-1.414 0-1.707.293s-.293.764-.293 1.707v8c0 .943 0 1.414.293 1.707s.764.293 1.707.293s1.414 0 1.707-.293s.293-.764.293-1.707m7-7v7c0 .943 0 1.414-.293 1.707s-.764.293-1.707.293s-1.414 0-1.707-.293s-.293-.764-.293-1.707v-7m.938-3.333V3.833m1.562 0V2.5m0 8V9.167M17.438 6.5h3.125m0 0c.517 0 .937.448.937 1v.667c0 .552-.42 1-.937 1H16.5M20.563 6.5c.517 0 .937-.448.937-1v-.667c0-.552-.42-1-.937-1H16.5M6.5 19.5v-5c0-.943 0-1.414-.293-1.707S5.443 12.5 4.5 12.5s-1.414 0-1.707.293s-.293.764-.293 1.707v5c0 .943 0 1.414.293 1.707s.764.293 1.707.293s1.414 0 1.707-.293s.293-.764.293-1.707"
+                                            color="currentColor" />
+                                    </svg>
+                                </span>
+                                <strong>1500</strong>
+                            </div>
+
+                            <div class="desktop">
+                                <h3>Sample Text</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
                         </div>
                     </div>
 
@@ -103,16 +151,64 @@ $temporalImage = builder_get_assets_path()."/images/temp.png";
                 <div class="zi-2 info-area">
                     <div class="infos">
                         <div class="info">
-                            <h3>Lorem</h3>
-                            <p>Lorem ipsum dolor</p>
+
+                            <div class="mobile">
+                                <span class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                        <g fill="none" stroke="currentColor" stroke-linecap="round"
+                                            stroke-linejoin="round" stroke-width="1.5" color="currentColor">
+                                            <path
+                                                d="M2 14c0-3.507 0-5.26.908-6.44q.25-.325.554-.592C4.57 6 6.212 6 9.5 6h5c3.288 0 4.931 0 6.038.968q.304.268.554.592C22 8.74 22 10.493 22 14s0 5.26-.908 6.44a4 4 0 0 1-.554.591C19.43 22 17.788 22 14.5 22h-5c-3.287 0-4.931 0-6.038-.968a4 4 0 0 1-.554-.592C2 19.26 2 17.507 2 14m14-8c0-1.886 0-2.828-.586-3.414S13.886 2 12 2s-2.828 0-3.414.586S8 4.114 8 6" />
+                                            <path
+                                                d="M12 11c-1.105 0-2 .672-2 1.5s.895 1.5 2 1.5s2 .672 2 1.5s-.895 1.5-2 1.5m0-6c.87 0 1.612.417 1.886 1M12 11v-1m0 7c-.87 0-1.612-.417-1.886-1M12 17v1m-6-6H2m20 0h-4" />
+                                        </g>
+                                    </svg>
+                                </span>
+
+                                <strong>150</strong>
+                            </div>
+                            <div class="desktop">
+                                <h3>Lorem</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
                         </div>
                         <div class="info">
-                            <h3>Lorem Ipsum</h3>
-                            <p>Lorem ipsum dolor</p>
+                            <div class="mobile">
+                                <span class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                        <g fill="none" stroke="currentColor" stroke-linecap="round"
+                                            stroke-linejoin="round" stroke-width="1.5" color="currentColor">
+                                            <path d="M4.5 9.5a7.5 7.5 0 1 0 15 0a7.5 7.5 0 0 0-15 0" />
+                                            <path
+                                                d="M9 10.167s.75 0 1.5 1.333c0 0 2.382-3.333 4.5-4m1.883 7.5l.67 3.21c.43 2.062.646 3.093.203 3.582c-.442.489-1.21.069-2.746-.772l-2.274-1.245c-.363-.198-.544-.298-.736-.298s-.373.1-.736.298L8.99 21.02c-1.536.84-2.304 1.261-2.746.772s-.227-1.52.203-3.582l.67-3.21" />
+                                        </g>
+                                    </svg>
+                                </span>
+                                <strong>152</strong>
+                            </div>
+
+                            <div class="desktop">
+                                <h3>Lorem</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
                         </div>
                         <div class="info">
-                            <h3>Sample Text</h3>
-                            <p>Lorem ipsum dolor</p>
+                            <div class="mobile">
+                                <span class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                        <path fill="none" stroke="currentColor" stroke-linecap="round"
+                                            stroke-linejoin="round" stroke-width="1.5"
+                                            d="M13.5 19.5v-8c0-.943 0-1.414-.293-1.707S12.443 9.5 11.5 9.5s-1.414 0-1.707.293s-.293.764-.293 1.707v8c0 .943 0 1.414.293 1.707s.764.293 1.707.293s1.414 0 1.707-.293s.293-.764.293-1.707m7-7v7c0 .943 0 1.414-.293 1.707s-.764.293-1.707.293s-1.414 0-1.707-.293s-.293-.764-.293-1.707v-7m.938-3.333V3.833m1.562 0V2.5m0 8V9.167M17.438 6.5h3.125m0 0c.517 0 .937.448.937 1v.667c0 .552-.42 1-.937 1H16.5M20.563 6.5c.517 0 .937-.448.937-1v-.667c0-.552-.42-1-.937-1H16.5M6.5 19.5v-5c0-.943 0-1.414-.293-1.707S5.443 12.5 4.5 12.5s-1.414 0-1.707.293s-.293.764-.293 1.707v5c0 .943 0 1.414.293 1.707s.764.293 1.707.293s1.414 0 1.707-.293s.293-.764.293-1.707"
+                                            color="currentColor" />
+                                    </svg>
+                                </span>
+                                <strong>1500</strong>
+                            </div>
+
+                            <div class="desktop">
+                                <h3>Sample Text</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
                         </div>
                     </div>
 
@@ -120,7 +216,6 @@ $temporalImage = builder_get_assets_path()."/images/temp.png";
 
                 <a href="#" class="zi-2 more"><span>More</span></a>
             </div>
-
 
             <div class="agent">
 
@@ -158,16 +253,64 @@ $temporalImage = builder_get_assets_path()."/images/temp.png";
                 <div class="zi-2 info-area">
                     <div class="infos">
                         <div class="info">
-                            <h3>Lorem</h3>
-                            <p>Lorem ipsum dolor</p>
+
+                            <div class="mobile">
+                                <span class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                        <g fill="none" stroke="currentColor" stroke-linecap="round"
+                                            stroke-linejoin="round" stroke-width="1.5" color="currentColor">
+                                            <path
+                                                d="M2 14c0-3.507 0-5.26.908-6.44q.25-.325.554-.592C4.57 6 6.212 6 9.5 6h5c3.288 0 4.931 0 6.038.968q.304.268.554.592C22 8.74 22 10.493 22 14s0 5.26-.908 6.44a4 4 0 0 1-.554.591C19.43 22 17.788 22 14.5 22h-5c-3.287 0-4.931 0-6.038-.968a4 4 0 0 1-.554-.592C2 19.26 2 17.507 2 14m14-8c0-1.886 0-2.828-.586-3.414S13.886 2 12 2s-2.828 0-3.414.586S8 4.114 8 6" />
+                                            <path
+                                                d="M12 11c-1.105 0-2 .672-2 1.5s.895 1.5 2 1.5s2 .672 2 1.5s-.895 1.5-2 1.5m0-6c.87 0 1.612.417 1.886 1M12 11v-1m0 7c-.87 0-1.612-.417-1.886-1M12 17v1m-6-6H2m20 0h-4" />
+                                        </g>
+                                    </svg>
+                                </span>
+
+                                <strong>150</strong>
+                            </div>
+                            <div class="desktop">
+                                <h3>Lorem</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
                         </div>
                         <div class="info">
-                            <h3>Lorem Ipsum</h3>
-                            <p>Lorem ipsum dolor</p>
+                            <div class="mobile">
+                                <span class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                        <g fill="none" stroke="currentColor" stroke-linecap="round"
+                                            stroke-linejoin="round" stroke-width="1.5" color="currentColor">
+                                            <path d="M4.5 9.5a7.5 7.5 0 1 0 15 0a7.5 7.5 0 0 0-15 0" />
+                                            <path
+                                                d="M9 10.167s.75 0 1.5 1.333c0 0 2.382-3.333 4.5-4m1.883 7.5l.67 3.21c.43 2.062.646 3.093.203 3.582c-.442.489-1.21.069-2.746-.772l-2.274-1.245c-.363-.198-.544-.298-.736-.298s-.373.1-.736.298L8.99 21.02c-1.536.84-2.304 1.261-2.746.772s-.227-1.52.203-3.582l.67-3.21" />
+                                        </g>
+                                    </svg>
+                                </span>
+                                <strong>152</strong>
+                            </div>
+
+                            <div class="desktop">
+                                <h3>Lorem</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
                         </div>
                         <div class="info">
-                            <h3>Sample Text</h3>
-                            <p>Lorem ipsum dolor</p>
+                            <div class="mobile">
+                                <span class="icon">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24">
+                                        <path fill="none" stroke="currentColor" stroke-linecap="round"
+                                            stroke-linejoin="round" stroke-width="1.5"
+                                            d="M13.5 19.5v-8c0-.943 0-1.414-.293-1.707S12.443 9.5 11.5 9.5s-1.414 0-1.707.293s-.293.764-.293 1.707v8c0 .943 0 1.414.293 1.707s.764.293 1.707.293s1.414 0 1.707-.293s.293-.764.293-1.707m7-7v7c0 .943 0 1.414-.293 1.707s-.764.293-1.707.293s-1.414 0-1.707-.293s-.293-.764-.293-1.707v-7m.938-3.333V3.833m1.562 0V2.5m0 8V9.167M17.438 6.5h3.125m0 0c.517 0 .937.448.937 1v.667c0 .552-.42 1-.937 1H16.5M20.563 6.5c.517 0 .937-.448.937-1v-.667c0-.552-.42-1-.937-1H16.5M6.5 19.5v-5c0-.943 0-1.414-.293-1.707S5.443 12.5 4.5 12.5s-1.414 0-1.707.293s-.293.764-.293 1.707v5c0 .943 0 1.414.293 1.707s.764.293 1.707.293s1.414 0 1.707-.293s.293-.764.293-1.707"
+                                            color="currentColor" />
+                                    </svg>
+                                </span>
+                                <strong>1500</strong>
+                            </div>
+
+                            <div class="desktop">
+                                <h3>Sample Text</h3>
+                                <p>Lorem ipsum dolor</p>
+                            </div>
                         </div>
                     </div>
 
@@ -175,6 +318,10 @@ $temporalImage = builder_get_assets_path()."/images/temp.png";
 
                 <a href="#" class="zi-2 more"><span>More</span></a>
             </div>
+
+
+
+
 
 
         </div>
