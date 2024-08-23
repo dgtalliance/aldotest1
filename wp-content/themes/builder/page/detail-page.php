d87154159a781d495bd66f2b73cb569ff3e4ced9
LaricoDGT <larico@dgtalliance.com>
2024-08-23 11:26:44 -0500

    detail page

---
+++ wp-content/themes/builder/page/detail-page.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Template Name: Detail Page
+ * Template Post Type: page
+ */
+get_header(); 
+
+?>
+
+
+
+
+<main>
+
+    <h1>Detail Page</h1>
+</main>
+
+
+
+
+
+<script>
+document.addEventListener("DOMContentLoaded", () => {
+
+
+});
+</script>
+
+
+<?php get_footer(); ?>
