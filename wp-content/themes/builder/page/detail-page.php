edca9589e6906cffea91eb7950430918d50f4f33
LaricoDGT <larico@dgtalliance.com>
2024-08-26 16:53:36 -0500

    full screen

--- wp-content/themes/builder/page/detail-page.php
+++ wp-content/themes/builder/page/detail-page.php
@@ -187,7 +187,8 @@ get_header();
 
 
     <div class="picture">
-        <img class="agent-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/agent.png" alt="" />
+        <img class="agent-image" src="<?php echo get_template_directory_uri(); ?>/assets/images/david-101-min.png"
+            alt="DS" />
 
         <div class="full-name">
             <h1 class="f-name"><span>Adrian</span> Somoza</h1>
